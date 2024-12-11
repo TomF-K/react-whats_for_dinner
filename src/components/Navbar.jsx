0bd57e8d47c719d5c18e2ee25c0801e7b87b5c45
Tom Forman-Kenrick <tom.kenrick@gmail.com>
2024-12-11 16:48:55 +0000

    updated nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { PiCookingPot } from "react-icons/pi";
 const Navbar = () => {
   return (
     <nav>
-          <Link to="/">
+          <Link to="/react-whats_for_dinner">
             <div className='title-wrapper'>
               <PiCookingPot className="title-icon"></PiCookingPot>
               <h1>What&apos;s for Dinner?</h1>
             </div>
         </Link>
     <ul className="nav-links">
-      <li><Link to="/">Home</Link></li>
+      <li><Link to="//react-whats_for_dinner">Home</Link></li>
       <li><Link to="/about">About</Link></li>
       {/* <li><Link to="/login">Login</Link></li> */}
     </ul>
